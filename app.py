4508027ed884759c684ba72c7b081c3541f6b630
SANTOSHKAMAL1 <git config --global user.email>
2025-07-08 13:13:44 +0530

    final version with app.py3

--- app.py
+++ app.py
@@ -137,4 +137,5 @@ def serve_pdf(filename):
 # -------------------------------
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+
